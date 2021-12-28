b3af2c0c0ae708c86ec83860bccda49df636dd1f
Adrian X <adrinium@gmail.com>
2021-12-28 10:20:44 +0200

    Add project files.

---
+++ BulkyBookWeb/Controllers/CategoriesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var objCategoryList = _context.Categories.ToList();
+
+            return View(objCategoryList);
+        }
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Categories.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+            return View(obj);
+        }
+    }
+}
